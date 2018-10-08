39df32d3ab8cff3115a16e41dc6584766def9dc9
GBTC406002UR <GBTC406002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-08 14:09:34 -0400

    first initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Main {
+
+    // public static void main(String[] args) {
+
+   // public class ListOddNumbers
+
+
+        public static void main(String[] args) {
+
+            int limit =20;
+            //int i;
+
+
+
+           // System.out.println("Printing Odd numbers between 1 and " + limit);
+
+            for (int i = 1; i <= limit; i++)
+                {
+                    if( i % 2 != 0)
+                    System.out.println(i + " ");
+                }
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
